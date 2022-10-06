81c69af083f00d87fea2af570deca760184bf11f
Prakash Shekhar <prakashshkhr@gmail.com>
2022-10-06 08:29:43 -0400

    finish multivariate integration extension

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ struct Bounds boundlst[N];
 double f(double xinputs[N]);
 double integrate(double a, double n);
 double montecarlo(int numvars, int iterations);
-void input(int rank, int world, int *numvars, double *pointer_a, double *pointer_b, double *pointer_c, double *pointer_d, int *pointer_iterations);
-void mpi_type(double *pointer_a, double *pointer_b, double *pointer_c, double *pointer_d, int *pointer_iterations, MPI_Datatype *mpi_input);
+void input(int rank, int world, int *numvars, int *pointer_iterations);
+void mpi_type(int *pointer_iterations, MPI_Datatype *mpi_input);
 double randomgen(struct Bounds bound);
 int main(int argc, char **argv)
 {
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world);
-    input(rank, world, &numvars, &boundlst[0].a, &boundlst[0].b, &boundlst[1].a, &boundlst[1].b, &n);
+    input(rank, world, &numvars, &n);
 
     if (rank == 0)
     {
@@ -62,10 +62,8 @@ double randomgen(struct Bounds bound)
 
 double f(double xinputs[N])
 {
-    // x0 = bound 1 rand num
-    // x1 = bound 2 rand num
     // given 0-2, 3-7, 4-5 from inside out evaluate to ~197.177
-    return pow(xinputs[0], 3) + sin(xinputs[1]) + pow(2, xinputs[3]);
+    return pow(xinputs[0], 3) + sin(xinputs[1]) + pow(2, xinputs[2]);
 }
 
 double montecarlo(int numvars, int iterations)
@@ -108,18 +106,18 @@ double montecarlo(int numvars, int iterations)
     return estimate;
 }
 
-void mpi_type(double *pointer_a, double *pointer_b, double *pointer_a1, double *pointer_b1, int *pointer_iterations, MPI_Datatype *mpi_input)
+void mpi_type(int *pointer_iterations, MPI_Datatype *mpi_input)
 {
-    MPI_Aint address_a, address_b, address_a1, address_b1, addresss_iterations;
-    MPI_Aint displacement_arr[5] = {0};
-    int blocklengths_arr[5] = {1, 1, 1, 1, 1};
-    MPI_Datatype types_arr[5] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, MPI_INT};
+    MPI_Aint addresss_iterations;
+    MPI_Aint displacement_arr[1] = {0};
+    int blocklengths_arr[1] = {1};
+    MPI_Datatype types_arr[1] = {MPI_INT};
     MPI_Get_address(pointer_iterations, &addresss_iterations);
-    displacement_arr[1] = address_b - address_a;
-    displacement_arr[2] = address_a1 - address_a;
-    displacement_arr[3] = address_b1 - address_a;
-    displacement_arr[4] = addresss_iterations - address_a;
-    MPI_Type_create_struct(5, blocklengths_arr, displacement_arr, types_arr, mpi_input);
+    // displacement_arr[1] = address_b - address_a;
+    // displacement_arr[2] = address_a1 - address_a;
+    // displacement_arr[3] = address_b1 - address_a;
+    // displacement_arr[4] = addresss_iterations - address_a;
+    MPI_Type_create_struct(1, blocklengths_arr, displacement_arr, types_arr, mpi_input);
     MPI_Type_commit(mpi_input);
 
     // displacement_arr[2] = addresss_iterations - address_a;
@@ -127,15 +125,19 @@ void mpi_type(double *pointer_a, double *pointer_b, double *pointer_a1, double *
     // MPI_Type_commit(mpi_input);
 }
 
-void input(int rank, int world, int *numvars, double *pointer_a, double *pointer_b, double *pointer_a1, double *pointer_b1, int *pointer_iterations)
+void input(int rank, int world, int *numvars, int *pointer_iterations)
 {
     MPI_Datatype mpi_input;
-    mpi_type(pointer_a, pointer_b, pointer_b1, pointer_b1, pointer_iterations, &mpi_input);
+    mpi_type(pointer_iterations, &mpi_input);
 
     if (rank == 0)
     {
         printf("Enter number of variables: \n");
         scanf("%d", numvars);
+        if(*numvars > N){
+            fprintf(stderr, "Too many variables! (max = 30)\n");
+            exit(1);
+        }
         printf("Enter number of iterations: \n");
         scanf("%d", pointer_iterations);
         for (int i = 0; i < *numvars; i++)
@@ -145,7 +147,7 @@ void input(int rank, int world, int *numvars, double *pointer_a, double *pointer
         }
     }
 
-    MPI_Bcast(pointer_a, 1, mpi_input, 0, MPI_COMM_WORLD);
+    // MPI_Bcast(pointer_a, 1, mpi_input, 0, MPI_COMM_WORLD);
 
     MPI_Type_free(&mpi_input);
 }
